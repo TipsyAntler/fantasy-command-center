813a1989f823bc0320ca476113d2b66c2b442b02
TipsyAntler <51962438+TipsyAntler@users.noreply.github.com>
2026-09-16 11:45:22 -0400

    Fix desktop FFCC logo asset

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -17,7 +17,7 @@ export default function Nav() {
   return (
     <nav className="app-nav" aria-label="Fantasy Command Center">
       <Link className="brand-block" href="/" aria-label="Fantasy Command Center home">
-        <div className="brand-mark"><img src="/fcc-icon.svg" alt="" aria-hidden="true" /></div>
+        <div className="brand-mark"><img src="/ffcc-icon.png" alt="" aria-hidden="true" /></div>
         <div className="brand-name">
           <span>FANTASY</span>
           <span className="command">COMMAND</span>
